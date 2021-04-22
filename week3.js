49a2a863021d1e95cf7c2123ee6f24d09085e8f3
amartin58 <adee1001@outlook.com>
2021-04-22 07:43:02 -0700

    Complete retrival of all totals

--- week3.js
+++ week3.js
@@ -1,13 +1,28 @@
-const getTotals = (num1, num2, num3) => {
-console.log(`\nnumbers pased to the function are: ${num1}, ${num2}, ${num3} `);
 
-let avgOfNums = (num1 + num2 + num3) / 3;
-console.log(`\naverage of numbers: ${avgOfNums}`);
+function allTotals (num1, num2, num3) { 
 
-let totalOfNums = num1 + num2 + num3;
-console.log(`\nsum of numbers: ${totalOfNums}`);
+   function getNums() {
+      console.log(`numbers passed to the function are: ${num1}, ${num2}, ${num3}`);
+   }
+   
+   function getAvg() {
+    let avg = (num1 + num2 + num3) / 3;
+    console.log(`avg of numbers: ${avg}`); 
+   }
 
-let productOfNums = num1 * num2 * num3;
-console.log(`\nproduct of numbers in the list: ${productOfNums}`)
+   function getSum() {
+      let sum = num1 + num2 + num3;
+console.log(`sum of numbers: ${sum}`);
 }
-getTotals(1, 3, 5)
+
+function getProduct() {
+   let product = num1 * num2 * num3;
+   console.log(`product of numbers in the list: ${product}`);
+}
+getNums()
+getAvg()
+getSum()
+getProduct()
+}
+
+allTotals(1, 3, 5)
