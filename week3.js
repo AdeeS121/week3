4e73f42e13ece92a1bac4f6bd7ab95450adc7594
amartin58 <adee1001@outlook.com>
2021-04-22 13:25:43 -0700

    Format document

--- week3.js
+++ week3.js
@@ -1,71 +1,27 @@
-
-
 function allTotals() {
-   num1 = 1;
-   num2 = 3;
-   num3 = 5;
-   getNums()
-   getAvg()
-   getSum()
-   getProduct()
+  num1 = 1;
+  num2 = 3;
+  num3 = 5;
+  getNums();
+  getAvg();
+  getSum();
+  getProduct();
 }
 
 function getNums() {
-   console.log(`numbers passed to the function are: ${num1}, ${num2}, ${num3}`);
-   }
-   
-   function getAvg() {
-    let avg = (num1 + num2 + num3) / 3;
-    console.log(`avg of numbers: ${avg}`); 
-   }
-
-   function getSum() {
-      let sum = num1 + num2 + num3;
-         console.log(`sum of numbers: ${sum}`);
+  console.log(`numbers passed to the function are: ${num1}, ${num2}, ${num3}`);
 }
-
+function getAvg() {
+  let avg = (num1 + num2 + num3) / 3;
+  console.log(`avg of numbers: ${avg}`);
+}
+function getSum() {
+  let sum = num1 + num2 + num3;
+  console.log(`sum of numbers: ${sum}`);
+}
 function getProduct() {
-   let product = num1 * num2 * num3;
-   console.log(`product of numbers in the list: ${product}`);
-  
+  let product = num1 * num2 * num3;
+  console.log(`product of numbers in the list: ${product}`);
 }
-return allTotals()
+return allTotals();
 
-console.log(allTotals())
-
-
-
-
-
-
-
-
-
-
-// function allTotals (num1, num2, num3) { 
-
-//    function getNums() {
-//       console.log(`numbers passed to the function are: ${num1}, ${num2}, ${num3}`);
-//    }
-   
-//    function getAvg() {
-//     let avg = (num1 + num2 + num3) / 3;
-//     console.log(`avg of numbers: ${avg}`); 
-//    }
-
-//    function getSum() {
-//       let sum = num1 + num2 + num3;
-// console.log(`sum of numbers: ${sum}`);
-// }
-
-// function getProduct() {
-//    let product = num1 * num2 * num3;
-//    console.log(`product of numbers in the list: ${product}`);
-// }
-// getNums()
-// getAvg()
-// getSum()
-// getProduct()
-// }
-
-// allTotals(1, 3, 5)
