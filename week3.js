bfb474ffc15946e3236396deee045b49634372cd
amartin58 <adee1001@outlook.com>
2021-04-22 11:31:15 -0700

    Unnested functions

--- week3.js
+++ week3.js
@@ -1,8 +1,17 @@
 
-function allTotals (num1, num2, num3) { 
+let num1 = 1
+let num2 = 3
+let num3 = 5
 
-   function getNums() {
-      console.log(`numbers passed to the function are: ${num1}, ${num2}, ${num3}`);
+function allTotals(){
+   getNums()
+   let average = getAvg()
+   let sum = getSum()
+   let product = getProduct()
+}
+
+function getNums() {
+   console.log(`numbers passed to the function are: ${num1}, ${num2}, ${num3}`);
    }
    
    function getAvg() {
@@ -12,17 +21,50 @@ function allTotals (num1, num2, num3) {
 
    function getSum() {
       let sum = num1 + num2 + num3;
-console.log(`sum of numbers: ${sum}`);
+         console.log(`sum of numbers: ${sum}`);
 }
 
 function getProduct() {
    let product = num1 * num2 * num3;
    console.log(`product of numbers in the list: ${product}`);
-}
-getNums()
-getAvg()
-getSum()
-getProduct()
+  
 }
 
-allTotals(1, 3, 5)
+console.log(allTotals())
+
+
+
+
+
+
+
+
+
+
+// function allTotals (num1, num2, num3) { 
+
+//    function getNums() {
+//       console.log(`numbers passed to the function are: ${num1}, ${num2}, ${num3}`);
+//    }
+   
+//    function getAvg() {
+//     let avg = (num1 + num2 + num3) / 3;
+//     console.log(`avg of numbers: ${avg}`); 
+//    }
+
+//    function getSum() {
+//       let sum = num1 + num2 + num3;
+// console.log(`sum of numbers: ${sum}`);
+// }
+
+// function getProduct() {
+//    let product = num1 * num2 * num3;
+//    console.log(`product of numbers in the list: ${product}`);
+// }
+// getNums()
+// getAvg()
+// getSum()
+// getProduct()
+// }
+
+// allTotals(1, 3, 5)
